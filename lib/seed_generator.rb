1ce822344d37c359b40509b4c510324126683c0e
Logan Weir <loganweir@gmail.com>
2016-09-29 12:29:14 -0700

    starting split testing

--- lib/seed_generator.rb
+++ lib/seed_generator.rb
@@ -81,40 +81,48 @@ feature_array = []
 factory = RGeo::Geographic.simple_mercator_factory(:srid => 4326)
 
 
+# Makes Testing Quicker
+test_map_data = map_data.each[0..1000]
+
+
 # Begin iterating through data
 map_data.each.with_index(1) do |item, index|
 
-	# Good for monitoring progress
-	puts "Starting item ##{index}! Left to go: #{(map_data.length - index)}"
+	# # Good for monitoring progress
+	# puts "Starting item ##{index}! Left to go: #{(map_data.length - index)}"
 
-	# Convert data into RGeo, then proper factory
-	rgeo_hash = RGeo::GeoJSON.decode(item['geometry'])
-	geo_data_projection = factory.collection([rgeo_hash])
+	if quat_id == 7926
 
-	zoom_hash.each do |zoom_level, zoom_params|
+		# Convert data into RGeo, then proper factory
+		rgeo_hash = RGeo::GeoJSON.decode(item['geometry'])
+		geo_data_projection = factory.collection([rgeo_hash])
 
-	 	# Skip if included in feature skip array
-		next if zoom_params['feature_skip'].include?\
-			(item['properties']['LIQ'])
+		puts geo_data_projection[0].exterior_ring.num_points
 
-		# Filters out polygons based on size and fill
-		next if zoom_test(geo_data_projection[0], 
-			zoom_params['size_fill_limits']) == false
+	# zoom_hash.each do |zoom_level, zoom_params|
 
-		# Begin simplification
-		projection = ProjectionFactory.new(geo_data_projection, factory)
+	#  	# Skip if included in feature skip array
+	# 	next if zoom_params['feature_skip'].include?\
+	# 		(item['properties']['LIQ'])
 
-		# Filter out holes that you can't see zoomed out
-		projection.hole_deleter(zoom_params['minimum_hole_size'])
+	# 	# Filters out polygons based on size and fill
+	# 	next if zoom_test(geo_data_projection[0], 
+	# 		zoom_params['size_fill_limits']) == false
 
-		# Simplify to a percentage of the original
-		simplfied_poly = \
-			projection.polygon_simplifier(zoom_params['simplification'])
+	# 	# Begin simplification
+	# 	projection = ProjectionFactory.new(geo_data_projection, factory)
 
-		# Adding to array
-		feature_array << feature_builder(layer_id, 
-				color_hash[item['properties']['LIQ']], 
-				zoom_level, simplfied_poly)
+	# 	# Filter out holes that you can't see zoomed out
+	# 	projection.hole_deleter(zoom_params['minimum_hole_size'])
+
+	# 	# Simplify to a percentage of the original
+	# 	simplfied_poly = \
+	# 		projection.polygon_simplifier(zoom_params['simplification'])
+
+	# 	# Adding to array
+	# 	feature_array << feature_builder(layer_id, 
+	# 			color_hash[item['properties']['LIQ']], 
+	# 			zoom_level, simplfied_poly)
 
 		
 	end
